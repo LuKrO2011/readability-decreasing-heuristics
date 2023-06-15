44c6269743204509c4eb90d7ef753f9216952671
LuKrO2011 <lukas.krodinger@gmail.com>
2023-06-15 10:46:54 +0200

    Adding second yaml configuration

--- src/main/java/de/uni_passau/fim/se2/rdm/config/RdcProbabilities.java
+++ src/main/java/de/uni_passau/fim/se2/rdm/config/RdcProbabilities.java
@@ -2,27 +2,6 @@ package de.uni_passau.fim.se2.rdm.config;
 
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
-
-// TODO: Use toml? https://toml.io/en/
-
-/*@Data
-public class RdcProbabilities {
-    private double doubleNewLine = 0.2;
-    private double noNewLine = 0.2;
-
-    // TODO: Distinguish inctab and dectab
-    private double doubleTab = 0.1;
-    private double noTab = 0.1;
-
-    private double doubleSpace = 0.2;
-    private double noSpace = 0.2;
-
-    private double newLineInsteadOfSpace = 0.2;
-    private double spaceInsteadOfNewline = 0.2;
-}*/
-
 @Data
 public class RdcProbabilities {
     private double doubleNewLine = 0;
--- src/main/java/de/uni_passau/fim/se2/rdm/config/YamlLoaderSaver.java
+++ src/main/java/de/uni_passau/fim/se2/rdm/config/YamlLoaderSaver.java
@@ -10,40 +10,40 @@ import java.io.InputStream;
 
 public class YamlLoaderSaver {
 
-  private static final String DEFAULT_CONFIG_FILE = "config.yaml";
-  private static final String CONFIG_FILE_PATH = "src/main/resources";
+    private static final String DEFAULT_CONFIG_FILE = "config.yaml";
+    private static final String CONFIG_FILE_PATH = "src/main/resources";
 
-  public Object load(String configFileName) {
-    try (InputStream inputStream = new FileInputStream(CONFIG_FILE_PATH + "/" + configFileName)) {
-      Yaml yaml = new Yaml();
-      return yaml.loadAs(inputStream, RdcProbabilities.class);
-    } catch (IOException e) {
-      e.printStackTrace();
+    public Object load(String configFileName) {
+        try (InputStream inputStream = new FileInputStream(CONFIG_FILE_PATH + "/" + configFileName)) {
+            Yaml yaml = new Yaml();
+            return yaml.loadAs(inputStream, RdcProbabilities.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-    return null;
-  }
 
-  public Object load() {
-    return load(DEFAULT_CONFIG_FILE);
-  }
-
-  public void save(String configFileName, Object object) {
-    try {
-      // Create DumperOptions to configure the output format
-      DumperOptions options = new DumperOptions();
-      options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-
-      // Create a YAML instance with the specified options
-      Yaml yaml = new Yaml(options);
-
-      // Convert the RdcProbabilities object to YAML and write it to the file
-      yaml.dump(object, new FileWriter(CONFIG_FILE_PATH + "/" + configFileName));
-    } catch (IOException e) {
-      e.printStackTrace();
+    public Object load() {
+        return load(DEFAULT_CONFIG_FILE);
     }
-  }
 
-  public void save(Object object) {
-    save(DEFAULT_CONFIG_FILE, object);
-  }
+    public void save(String configFileName, Object object) {
+        try {
+            // Create DumperOptions to configure the output format
+            DumperOptions options = new DumperOptions();
+            options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+
+            // Create a YAML instance with the specified options
+            Yaml yaml = new Yaml(options);
+
+            // Convert the RdcProbabilities object to YAML and write it to the file
+            yaml.dump(object, new FileWriter(CONFIG_FILE_PATH + "/" + configFileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void save(Object object) {
+        save(DEFAULT_CONFIG_FILE, object);
+    }
 }
