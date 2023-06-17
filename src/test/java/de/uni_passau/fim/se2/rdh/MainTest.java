a00d900682e5fba34ddf21592120da848b730637
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-06-17 10:01:43 +0200

    testRunHelp is working

--- src/test/java/de/uni_passau/fim/se2/rdh/MainTest.java
+++ src/test/java/de/uni_passau/fim/se2/rdh/MainTest.java
@@ -5,11 +5,16 @@
 package de.uni_passau.fim.se2.rdh;
 
 import de.uni_passau.fim.se2.rdh.util.IOTest;
+import de.uni_passau.fim.se2.rdh.util.Randomness;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import picocli.CommandLine;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,23 +23,52 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MainTest extends IOTest {
+class MainTest { // extends IOTest {
 
     private static final String ECHO_COMMAND_NAME = "echo";
 
+    protected CommandLine commandLine;
+
+    private PrintStream originalOutput;
+    private PrintStream originalError;
+    private InputStream originalInput;
+    protected ByteArrayOutputStream newOutput;
+    protected ByteArrayOutputStream newError;
+
+    protected static final String NEWLINE = System.lineSeparator();
+
+    @BeforeEach
+    void setupStdinStdout() {
+        Randomness.setSeed(0);
+        originalOutput = System.out;
+        newOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(newOutput, true, StandardCharsets.UTF_8));
+
+        originalError = System.err;
+        newError = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(newError, true, StandardCharsets.UTF_8));
+
+        originalInput = System.in;
+    }
+
+    @BeforeEach
+    void setupCommandLine() {
+        commandLine = new CommandLine(new Main());
+    }
+
     private static final String HELP_TEXT = "" +
-            "Usage: readability-decreasing-heuristics [-hV] [-o, --output=<outputPath>]\n" +
+            "Usage: readability-decreasing-heuristics [-hV] [-o=<outputPath>]\n" +
             "       [--seed=<seed>] <inputPath>\n" +
             "Heuristics for decreasing the readability of Java source code.\n" +
             "      <inputPath>     The path to the input. Can be a file or a directory.\n" +
             "  -h, --help          Show this help message and exit.\n" +
-            "      -o, --output=<outputPath>\n" +
+            "  -o, --output=<outputPath>\n" +
             "                      The path to the output directory. If not specified, the\n" +
             "                        output is written into the directory of the input\n" +
             "                        (file).\n" +
             "      --seed=<seed>   A number that is used as seed to initialize the random\n" +
             "                        instance to allow for reproducible runs.\n" +
-            "  -V, --version       Print version information and exit.";
+            "  -V, --version       Print version information and exit.\n";
 
 
     @Test
@@ -47,8 +81,13 @@ class MainTest extends IOTest {
         execute(0, "-h");
 
         // Assert that the help text was printed
-        // assertOutputContains(HELP_TEXT);
+        assertThat(newOutput.toString(StandardCharsets.UTF_8)).isEqualTo(normalise(HELP_TEXT));
     }
+
+    private static String normalise(final String s) {
+        return s.replace("\n", NEWLINE);
+    }
+
 
     @Test
     void testRunInputFile(@TempDir Path tmpDir) {
@@ -74,6 +113,8 @@ class MainTest extends IOTest {
     }
 
     private void execute(int expectedExitCode, String... args) {
+        commandLine = new CommandLine(new Main());
+        
         int exitCode = commandLine.execute(args);
         assertThat(exitCode).isEqualTo(expectedExitCode);
     }
