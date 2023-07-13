4f6bb830f365541f2e2bfc3710b135f1e644690e
lukas <lukas.krodinger@gmail.com>
2023-07-13 12:22:59 +0200

    Adding regex for valid java method names

--- src/main/java/de/uni_passau/fim/se2/rdh/refactorings/rename/CtRenameMethodRefactoring.java
+++ src/main/java/de/uni_passau/fim/se2/rdh/refactorings/rename/CtRenameMethodRefactoring.java
@@ -4,21 +4,14 @@ import spoon.refactoring.AbstractRenameRefactoring;
 import spoon.refactoring.RefactoringException;
 import spoon.reflect.code.CtInvocation;
 import spoon.reflect.declaration.CtElement;
-import spoon.reflect.declaration.CtExecutable;
 import spoon.reflect.declaration.CtMethod;
-import spoon.reflect.declaration.CtVariable;
 import spoon.reflect.reference.CtExecutableReference;
-import spoon.reflect.reference.CtReference;
 import spoon.reflect.visitor.chain.CtConsumer;
 import spoon.reflect.visitor.filter.*;
-import spoon.support.reflect.declaration.CtExecutableImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
-
-// TODO: Add checks for valid method names
-// TODO: Use AllMethodsSameSignatureFunction?
 
 /**
  * This class implements the refactoring for renaming a method.
@@ -28,13 +21,13 @@ public class CtRenameMethodRefactoring extends AbstractRenameRefactoring<CtMetho
     /**
      * This regular expression matches all valid variable names.
      */
-    private static final Pattern VALID_VARIABLE_NAME_REGEX = javaIdentifierRE;
+    private static final Pattern VALID_METHOD_NAME = Pattern.compile("[a-zA-Z_$][a-zA-Z0-9_$]*");
 
     /**
      * This constructor sets the regular expression for valid variable names.
      */
     public CtRenameMethodRefactoring() {
-        super(VALID_VARIABLE_NAME_REGEX);
+        super(VALID_METHOD_NAME);
     }
 
     /**
@@ -48,7 +41,10 @@ public class CtRenameMethodRefactoring extends AbstractRenameRefactoring<CtMetho
         ExecutableReferenceFilter execRefFilter = new ExecutableReferenceFilter();
         final List<CtInvocation<?>> invocations = new ArrayList<>();
 
-        // TODO: Use filter instead for all methods with same signature? @see CtRenameLocalVariableRefactoring
+        /*
+         * TODO: Use filter instead for all methods with same signature? @see CtRenameLocalVariableRefactoring,
+         *  @see VariableReferenceFunction. Consider super/subclasses?
+         */
         // Add own executable
         execRefFilter.addExecutable(target.getReference().getExecutableDeclaration());
 
@@ -76,13 +72,15 @@ public class CtRenameMethodRefactoring extends AbstractRenameRefactoring<CtMetho
      */
     @Override
     protected void checkNewNameIsValid() {
-        // TODO: Implement using a own regex for javaIdentifierRE
+        if (!VALID_METHOD_NAME.matcher(newName).matches()) {
+            throw new RefactoringException("Invalid method name: " + newName);
+        }
     }
 
     /**
      * {@inheritDoc}
      * There is a name conflict if there is a method with the same name and signature
-     * TODO: Check for lambdas, anonymous classes, overriding methods etc.
+     * TODO: Check for super/subclass conflicts
      */
     @Override
     protected void detectNameConflicts() {
@@ -109,7 +107,6 @@ public class CtRenameMethodRefactoring extends AbstractRenameRefactoring<CtMetho
 
     /**
      * This method creates a name conflict issue for the given method.
-     * TODO: Catch the exception?
      *
      * @param conflictMethod The method with the same name and signature
      */
